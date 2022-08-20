5a9043b4eb4dde4b85854b7028ca152e63d3c184
Kring <746822492@qq.com>
2022-08-20 23:02:37 +0800

    添加属性

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const strTempl = `
-    <div>
+    <div class="box parent" key="1" disabled>
         <p>
             <h1>内容1</h1>
             <p>内容2</p>
             <ul>
-                <li>A</li>
-                <li>B</li>
-                <li>C</li>
+                <li key="A">A</li>
+                <li key="B">B</li>
+                <li key="C">C</li>
             </ul>
         </p>
     </div>
@@ -17,18 +17,19 @@ const parseStrTempl = str => {
     // let contentStartIdx = 0
     // stack1存放标签，stack2存放子节点
     const [stack1, stack2] = [[], []]
-    const tagStartReg = /^\<([a-z]+[1-6]?)\>/
+    const tagStartReg = /^\<([a-z]+[1-6]?)([^\<]*)\>/
     const tagContentReg = /^([^\<^\n]+)\<\/([a-z]+[1-6]?)\>/
     const tagEndReg = /^\<\/([a-z]+[1-6]?)\>/
     while (tempStr.length) {
         if (tempStr.startsWith(' ')) {
             index++
         } else if (tagStartReg.test(tempStr)) { // 匹配到开始标签
-            // console.log('匹配到开始标签')
-            const [, tagName] = tempStr.match(tagStartReg)
+            const [, tagName, attrsStr] = tempStr.match(tagStartReg)
+            const attrs = parseAttrs(attrsStr)
+            console.log('匹配到开始标签', tagName, attrs)
             stack1.push(tagName)
-            stack2.push({ tag: tagName, children: [] })
-            index += tagName.length + 2
+            stack2.push({ tagName, attrs, children: [] })
+            index += tagName.length + attrsStr.length + 2
             // contentStartIdx = index
         } else if (tagEndReg.test(tempStr)) {
             // console.log('匹配到结束标签')
@@ -49,12 +50,39 @@ const parseStrTempl = str => {
             index += text.length
         } else {
             console.log('没匹配到')
-            // console.log(tempStr)
             index++
         }
         tempStr = str.substring(index)
     }
     console.log(stack1, stack2[0])
+}
+
+function parseAttrs (attrs) {
+    // class="box parent" key="1"
+    const trimedAttrs = attrs.trim()
+    if (!trimedAttrs) return []
+    let [startIdx, idx, isInQuote, arr] = [0, 0, false, []]
+    const generateAttrObj = () => {
+        if (startIdx >= idx) return
+        const [key, val] = trimedAttrs.substring(startIdx, idx).split('=')
+        arr.push({
+            name: key,
+            value: val === void 0 ? true : val // 单属性(disabled这种)时默认赋值为true
+        })
+    }
+    while (idx < trimedAttrs.length) {
+        if (trimedAttrs[idx] === '"') {
+            isInQuote = !isInQuote
+        } else if (trimedAttrs[idx] === ' ') {
+            if (!isInQuote) {
+                generateAttrObj()
+                startIdx = idx + 1
+            }
+        }
+        idx++
+    }
+    generateAttrObj()
+    return arr
 }
 
 parseStrTempl(strTempl)
